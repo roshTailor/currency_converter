7347dac006575978b8fc0f77576c69f8d6e77656
roshni <roshnitailor47@gmail.com>
2023-01-29 12:56:14 +0530

    Initial commit
    Roshni Lungiwala 30/01/2023 09:27

--- lib/models/currencies.dart
+++ lib/models/currencies.dart
@@ -4,8 +4,34 @@
 
 import 'dart:convert';
 
-Map<String, String> currencyFromJson(String str) =>
-    Map.from(json.decode(str)).map((k, v) => MapEntry<String, String>(k, v));
+Map<String, String> currencyFromJson(String str) => Map.from(json.decode(str)).map((k, v) => MapEntry<String, String>(k, v));
 
-String currencyToJson(Map<String, String> data) =>
-    json.encode(Map.from(data).map((k, v) => MapEntry<String, dynamic>(k, v)));
+String currencyToJson(Map<String, String> data) => json.encode(Map.from(data).map((k, v) => MapEntry<String, dynamic>(k, v)));
+
+// To parse this JSON data, do
+//
+//     final allCurrencies = allCurrenciesFromJson(jsonString);
+
+AllCurrencies allCurrenciesFromJson(String str) => AllCurrencies.fromJson(json.decode(str));
+
+String allCurrenciesToJson(AllCurrencies data) => json.encode(data.toJson());
+
+class AllCurrencies {
+  AllCurrencies({
+    this.success,
+    this.currencies,
+  });
+
+  bool? success;
+  Map<String, String>? currencies;
+
+  factory AllCurrencies.fromJson(Map<String, dynamic> json) => AllCurrencies(
+        success: json["success"],
+        currencies: Map.from(json["currencies"]!).map((k, v) => MapEntry<String, String>(k, v)),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "success": success,
+        "currencies": Map.from(currencies!).map((k, v) => MapEntry<String, dynamic>(k, v)),
+      };
+}
--- lib/services/httpservice.dart
+++ lib/services/httpservice.dart
@@ -1,30 +1,54 @@
+import 'dart:convert';
 
 import 'package:currency_converter/models/currencies.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
 
+import '../apiUtils.dart';
 import '../models/currencyRate.dart';
 
 Future<CurrencyRate> getRate() async {
-  var response = await http.get(Uri.parse(
-      'https://openexchangerates.org/api/latest.json?base=USD&app_id=b468b5c301764177baaa2bb72a05f248'));
+  var response = await http.get(Uri.parse('https://openexchangerates.org/api/latest.json?base=USD&app_id=b468b5c301764177baaa2bb72a05f248'));
   final result = currencyRateFromJson(response.body);
   return result;
 }
 
 Future<Map> getCurrencies() async {
-  var response = await http.get(Uri.parse(
-      'https://openexchangerates.org/api/currencies.json?app_id=b468b5c301764177baaa2bb72a05f248'));
+  var response = await http.get(Uri.parse('https://openexchangerates.org/api/currencies.json?app_id=b468b5c301764177baaa2bb72a05f248'));
   final allCurrencies = currencyFromJson(response.body);
   return allCurrencies;
 }
 
-String convert(Map<String,double> exchangeRates, String amount, String baseCur,
-    String finalCur) {
-  String output = (double.parse(amount) /
-      exchangeRates[baseCur]! *
-      exchangeRates[finalCur]!)
-      .toStringAsFixed(2)
-      .toString();
+String convert(Map<String, double> exchangeRates, String amount, String baseCur, String finalCur) {
+  String output = (double.parse(amount) / exchangeRates[baseCur]! * exchangeRates[finalCur]!).toStringAsFixed(2).toString();
   return output;
 }
+
+Future<List<AllCurrencies>> allCurrencies() async {
+  Response response = await get(Uri.parse(API.CurrencyAPI));
+  if (response.statusCode == 200) {
+    List<dynamic> currencies = jsonDecode(response.body);
+    List<AllCurrencies> allCurr = currencies.map((e) => AllCurrencies.fromJson(e)).toList();
+    return allCurr;
+  } else {
+    throw "Unable to retrieve.";
+  }
+}
+
+// Future<List<User>> getPosts() async {
+//   Response res = await get(Uri.parse(postsURL));
+//
+//   if (res.statusCode == 200) {
+//     List<dynamic> body = jsonDecode(res.body);
+//
+//     List<User> posts = body
+//         .map(
+//           (dynamic item) => User.fromJson(item),
+//     )
+//         .toList();
+//
+//     return posts;
+//   } else {
+//     throw "Unable to retrieve posts.";
+//   }
+// }
